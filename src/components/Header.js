2fffc9c2b11f4bc50465d60fcde7feb9c3e74e1a
Pantelis <tampantelis@gmail.com>
2019-07-05 10:29:11 +0300

    styled header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
   return (
-   <Text>Albums</Text>
+    <View style={viewStyle} >
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
   );
+};
+
+const styles = {
+    viewStyle: {
+      backgroundColor: '#F8F8F8',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 60,
+      paddingTop: 15,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+      position: 'relative'
+    },
+    textStyle:{
+      fontSize: 20
+    }
 };
 
 export default Header;
